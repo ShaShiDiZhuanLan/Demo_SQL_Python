9cd212e8620284a03b7e98a63e1a42cd00580520
shazhenyu <qq190957403@163.com>
2020-04-08 10:30:06 +0800

    更新mongodb

--- Demo_mongodb.py
+++ Demo_mongodb.py
@@ -8,149 +8,167 @@ Url: https://shazhenyu.blog.csdn.net/article/details/92841495
 """
 from pymongo import MongoClient
 
-# 数据库列表
-def ifnotDB(my_client, db_name):
-    db_list = my_client.list_database_names()
-    print("数据库列表:",db_list)
-    if db_name in db_list:
-        print("%s 数据库已存在！"%db_name)
-    else:
-        print("%s 数据库不存在！"%db_name)
 
-# 测试数据库是否连接成功 返回测试结果
-def testDB(my_client):
-    db = my_client.test
-    print("db:", db)
+def is_exists_db(client, name):
+    """数据库列表"""
+    db_list = client.list_database_names()
+    print("数据库列表:", db_list)
+    if name in db_list:
+        print("%s 数据库已存在！" % name)
+    else:
+        print("%s 数据库不存在！" % name)
+
+
+def test_db(client):
+    """测试数据库是否连接成功 返回测试结果"""
+    db = client.test
+    print("test_db: ", db)
     return db
 
-# 创建一个数据库 返回数据库对象
-def creatDB(my_client, name):
-    my_db = my_client[name]
-    ifnotDB(my_client, name)
-    return my_db
 
-# 表单列表
-def ifnotCol(my_db, col_name):
-    col_list = my_db.list_collection_names()
-    if col_name in col_list:
-        print("%s 集合已存在！"%col_name)
+def create_db(client, name):
+    """创建一个数据库 返回数据库对象"""
+    db = client[name]
+    is_exists_db(client, name)
+    return db
+
+
+def is_exists_col(db, name):
+    """表单列表"""
+    col_list = db.list_collection_names()
+    if name in col_list:
+        print("%s 集合已存在！" % name)
     else:
-        print("%s 集合不存在！"%col_name)
+        print("%s 集合不存在！" % name)
 
-# 创建一个表单 返回表单对象
-def creatCol(my_db, name):
-    sheet_tab_one = my_db[name]
-    ifnotCol(my_db, name)
+
+def create_col(db, name):
+    """创建一个表单 返回表单对象"""
+    sheet_tab_one = db[name]
+    is_exists_col(db, name)
     return sheet_tab_one
 
-# 增加一条数据 返回_id
-def addData_one(my_col, my_json):
-    result = my_col.insert_one(my_json)
-    print(result.inserted_id,"增加一条数据")# 返回 _id 字段
+
+def add_one_data(col, json):
+    """增加一条数据 返回_id"""
+    result = col.insert_one(json)
+    print("增加一条数据: ", result.inserted_id)  # 返回 _id 字段
     return result.inserted_id
 
-# 增加多条数据 返回所有文档对应的 _id 值
-def addData_many(my_col, my_jsons):
-    result = my_col.insert_many(my_jsons)
-    print(result.inserted_ids)
+
+def add_many_data(col, jsons):
+    """增加多条数据 返回所有文档对应的 _id 值"""
+    result = col.insert_many(jsons)
+    print("增加多条数据: ", result.inserted_ids)
     return result.inserted_ids
 
-# 删除一条数据
-def delData_one(my_col, myquery):
-    result = my_col.delete_one(myquery)
-    print(result.deleted_count,"数据已删除")
 
-# 删除多条数据
-def delData_many(my_col, myquery):
-    result = my_col.delete_many(myquery)
-    print(result.deleted_count,"数据已删除")
+def del_one_data(col, query):
+    """删除一条数据"""
+    result = col.delete_one(query)
+    print("数据已删除: ", result.deleted_count)
 
-# 删除一个表单
-def delCol(my_col):
-    my_col.drop()
+
+def del_many_data(col, query):
+    """删除多条数据"""
+    result = col.delete_many(query)
+    print("数据已删除: ", result.deleted_count)
+
+
+def del_col(col):
+    """删除一个表单"""
+    col.drop()
     print("删除表单成功")
 
-# 修改匹配到的第一条数据
-def updateData_one(my_col, myquery, new_val):
-    result = my_col.update_one(myquery, new_val)
-    print(result.modified_count, "文档已修改")
 
-# 修改匹配到的多条数据
-def updateData_many(my_col, myquery, new_val):
-    result = my_col.update_many(myquery, new_val)
-    print(result.modified_count, "文档已修改")
+def update_one_data(col, query, new_val):
+    """修改匹配到的第一条数据"""
+    result = col.update_one(query, new_val)
+    print("文档已修改: ", result.modified_count)
 
-# 按照某字段排序 默认True是正序，False是倒序
-def sortData(my_col, my_key, sortB = True):
-    if sortB:
-        my_doc = my_col.find().sort(my_key)
+
+def update_many_data(col, query, new_val):
+    """修改匹配到的多条数据"""
+    result = col.update_many(query, new_val)
+    print("文档已修改: ", result.modified_count)
+
+
+def sort_data(col, key, sort_b=True):
+    """按照某字段排序 默认True是正序，False是倒序"""
+    if sort_b:
+        my_doc = col.find().sort(key)
     else:
-        my_doc = my_col.find().sort(my_key, -1)
+        my_doc = col.find().sort(key, -1)
     for item in my_doc:
         print(item)
 
-# 查询一条数据
-def searchData_one(my_col):
-    result = my_col.find_one()
-    print(result)
 
-# 查询集合中所有数据
-def searchData_many(my_col, limit = 0):
-    for item in my_col.find().limit(limit):
+def search_one_data(col):
+    """查询一条数据"""
+    result = col.find_one()
+    print("查询一条数据: ", result)
+
+
+def search_many_data(col, limit=0):
+    """查询集合中所有数据"""
+    for item in col.find().limit(limit):
         print(item)
 
-# 根据指定条件查询 或者正则表达式查询（比如 my_query = { "name": { "$regex": "^S" } }）
-def searchData_miss(my_col, my_query):
-    print("根据指定条件查询: ", my_query)
-    my_doc = my_col.find(my_query)
+
+def search_miss_data(col, query):
+    """根据指定条件查询 或者正则表达式查询（比如 my_query = { "name": { "$regex": "^S" } }）"""
+    print("根据指定条件查询: ", query)
+    my_doc = col.find(query)
     for item in my_doc:
         print(item)
+
 
 if __name__ == "__main__":
-    client_ip = "193.112.61.11"
+    client_ip = "127.0.0.1"
     client_port = 27017
     db_name = 'mongodb_test'
     col_name = 'col_test'
     my_json = {"name": "ShaShiDi", "url": "https://shazhenyu.blog.csdn.net/"}
     # 可以指定_id 我们也可以自己指定 id，插入
-    my_jsons = [{"_id": 1,"name": "sha", "url": "https://www.shazhenyu.com"},{"_id": 2,"name": "sha2", "url": "https://shazhenyu.com"}]
+    my_jsons = [{"_id": 1, "name": "sha", "url": "https://www.shazhenyu.com"},
+                {"_id": 2, "name": "sha2", "url": "https://shazhenyu.com"}]
     my_query = {"name": "ShaShiDi"}
     my_query_regex = {"name": {"$regex": "^S"}} # 以下实例用于读取 name 字段中第一个字母为 "S" 的数据
     my_key = "_id"
     new_values = {"$set": {"name": "ShaShiDi_new"}}
 
     my_client = MongoClient(client_ip, client_port)
-    testDB(my_client)
-    my_db = creatDB(my_client,db_name)
-    my_col = creatCol(my_db,col_name)
+    test_db(my_client)
+    my_db = create_db(my_client,db_name)
+    my_col = create_col(my_db,col_name)
 
     # # 增加一条数据
-    # addData_one(my_col, my_json)
+    # add_one_data(my_col, my_json)
     # # 增加多条数据
-    # addData_many(my_col, my_jsons)
+    # add_many_data(my_col, my_jsons)
 
     # # 删除一个符合条件的集合
-    # delData_one(my_col, my_query)
+    # del_one_data(my_col, my_query)
     # # 删除所有符合条件的集合
-    # delData_many(my_col, my_query)
+    # del_many_data(my_col, my_query)
     # # 删除该集合中的所有文档
-    # delData_many(my_col, {})
+    # del_many_data(my_col, {})
     # # 删除表单
-    # delCol(my_col)
+    # del_col(my_col)
 
     # # 修改第一个匹配到的文档
-    # updateData_one(my_col, my_query, new_values)
+    # update_one_data(my_col, my_query, new_values)
     # # 修改所有符合条件的文档
-    # updateData_many(my_col, my_query, new_values)
+    # update_many_data(my_col, my_query, new_values)
 
     # # 按照某字段排序 默认True是正序，False是倒序
-    # sortData(my_col, my_key)
+    # sort_data(my_col, my_key)
 
     # # 根据指定条件查询
-    # searchData_miss(my_col, my_query)
+    # search_miss_data(my_col, my_query)
     # # 根据正则表达式查询
-    # searchData_miss(my_col, my_query_regex)
+    # search_miss_data(my_col, my_query_regex)
 
     # # 查询集合中所有数据 如果写第二个参数，就是指定条数记录查询
-    # # searchData_many(my_col)
-    # searchData_many(my_col,2)
+    # # search_many_data(my_col)
+    # search_many_data(my_col,2)
